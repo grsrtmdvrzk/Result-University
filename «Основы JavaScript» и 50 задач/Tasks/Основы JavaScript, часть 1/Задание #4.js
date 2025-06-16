f28899f97f5dbfc28f9b789f1631a1ac2911c38c
grsrtmdvrzk <grsrtmdvrzk@gmail.com>
2025-06-17 00:48:25 +0400

    Add files via upload

---
+++ «Основы JavaScript» и 50 задач/Tasks/Основы JavaScript, часть 1/Задание #4.js
@@ -0,0 +1,22 @@
+// TASK
+//     Выведите в консоль следующие выражения:
+//         - typeof 'Hello'
+//         - typeof 10
+//         - typeof {}
+//         - typeof null
+//         - typeof undefined
+//         - typeof true
+//         - typeof []
+//         - typeof (() => {})
+//
+// Проанализируйте результат выполнения программы и определите для себя, почему произошел тот или иной вывод.
+
+// SOLUTION
+console.log(typeof 'Hello')
+console.log(typeof 10)
+console.log(typeof {})
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof true)
+console.log(typeof [])
+console.log(typeof (() => {}))
