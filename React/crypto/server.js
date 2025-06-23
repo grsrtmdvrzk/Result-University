5d5f7800d60d7eab098dc32a2a471d3dd2e32dde
grsrtmdvrzk <grsrtmdvrzk@gmail.com>
2025-06-23 19:26:46 +0400

    Add files via upload

--- React/crypto/server.js
+++ React/crypto/server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 80
 
-app.use(express.static('dist'))
+app.use(express.static('frontend/dist'))
 
 app.listen(port, () => console.log('Server has been started on port 80...'))
