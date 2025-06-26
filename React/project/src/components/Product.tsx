2167a72e13b4f8a5274b7a06a823518d193ab05f
grsrtmdvrzk <grsrtmdvrzk@gmail.com>
2025-06-26 23:57:13 +0400

    Add files via upload

---
+++ React/project/src/components/Product.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export function Product(props: any) {
+    return (
+        <div
+            className="border py-2 px-4 rounded flex flex-col items-center mb-2"
+        >
+            Product!
+        </div>
+    )
+}
