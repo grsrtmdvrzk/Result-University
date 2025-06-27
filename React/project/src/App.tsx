532931f7b8ffd1880f7727dd6e426de397324401
grsrtmdvrzk <grsrtmdvrzk@gmail.com>
2025-06-27 23:46:52 +0400

    Add files via upload

--- React/project/src/App.tsx
+++ React/project/src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
 import {Product} from './components/Product'
-import {products} from './data/products'
+import {useProducts} from './hooks/products'
+import {Loader} from './components/Loader'
+import {ErrorMessage} from './components/ErrorMessage'
+import {Modal} from './components/Modal'
+import {CreateProduct} from './components/CreateProduct'
 
 function App() {
+    const {loading, error, products} = useProducts()
+
     return (
         <div className="container mx-auto max-w-2xl pt-5">
-            <Product product={products[0]} />
+            { loading &&  <Loader /> }
+            { error && <ErrorMessage error={error} /> }
+            { products.map(product => <Product product={product} key={product.id} />) }
+
+            <Modal title="Create new product">
+                <CreateProduct />
+            </Modal>
         </div>
     )
 }
