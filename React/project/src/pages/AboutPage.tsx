d73bb01808ea9d5b3e22564052f2719cbab43f2e
grsrtmdvrzk <grsrtmdvrzk@gmail.com>
2025-06-28 07:41:13 +0400

    Add files via upload

---
+++ React/project/src/pages/AboutPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function AboutPage() {
+    return (
+        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae consectetur dolore dolorem doloremque, eius fugiat impedit ipsa labore, minus quidem rem reprehenderit, saepe sunt. Consectetur minus odio repudiandae tempora totam? Asperiores assumenda cum delectus distinctio, excepturi magni maxime mollitia necessitatibus nemo nisi obcaecati odit quis quod ullam voluptate voluptatem voluptatum.</div>
+    )
+}
